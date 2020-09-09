fdb56fbd43e595164518510e2a02c5051e42d4cb
Flavio Cintra <flavio.cintra99@gmail.com>
2020-09-09 20:56:36 -0300

    urlconf e templates de html

--- src/webburguer_app/views.py
+++ src/webburguer_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+
+from webburguer_app.models import Franquia, Franqueada, BurguerUser, Produto, Pedido, PedidoProduto
+
+def index(request):
+    return render(request, 'webburguer_app/home.html')
