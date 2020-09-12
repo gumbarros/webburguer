8577ed7f3a649fbd496fbbab12f15ff9ba1ea106
Gustavo Mauricio de Barros <gustavomauriciodebarros@gmail.com>
2020-09-12 10:47:15 -0300

    index.html

--- src/webburguer_app/views.py
+++ src/webburguer_app/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-
 from webburguer_app.models import Franquia, Franqueada, BurguerUser, Produto, Pedido, PedidoProduto
 
 def index(request):
-    return render(request, 'webburguer_app/home.html')
+    return render(request, 'index.html')
